e5284a2903dae33644a0342679e57da5e68ae7bd
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-11-30 13:29:20 -0500

    Use errorHandler.

--- tests/10-api.spec.js
+++ tests/10-api.spec.js
@@ -12,6 +12,12 @@ chai.use(chaiHttp);
 describe('ezcap-express', () => {
   describe('authorizeZcapInvocation', () => {
     const app = express();
+
+    // eslint-disable-next-line no-unused-vars
+    function errorHandler(err, req, res, next) {
+      res.status(500).send(err.message);
+    }
+
     app.post('/documents',
       authorizeZcapInvocation({
         documentLoader: wrappedDocumentLoader,
@@ -30,11 +36,11 @@ describe('ezcap-express', () => {
       }),
       // eslint-disable-next-line no-unused-vars
       (req, res, next) => {
-        res.json(
-          req.clientMetadata
-        );
+        res.json(req.clientMetadata);
       }
     );
+    app.use(errorHandler);
+
     let requester;
     before(async () => {
       requester = chai.request(app).keepOpen();
@@ -42,39 +48,11 @@ describe('ezcap-express', () => {
     after(async () => {
       requester.close();
     });
-    it('should throw internal server error', async () => {
-    // eslint-disable-next-line no-unused-vars
-    //   const zcap = {
-    //     '@context': [
-    //       'https://w3id.org/zcap/v1',
-    //       'https://w3id.org/security/suites/ed25519-2020/v1'
-    //     ],
-    //     id: 'urn:zcap:delegated:z9gLKoFmKHwhxCzmo91Ywnh',
-    // eslint-disable-next-line max-len
-    //     parentCapability: 'urn:zcap:root:https%3A%2F%2Fexample.com%2Fdocuments',
-    //     invocationTarget: 'https://example.com/documents',
-    // eslint-disable-next-line max-len
-    //     controller: 'did:key:z6MknBxrctS4KsfiBsEaXsfnrnfNYTvDjVpLYYUAN6PX2EfG',
-    //     expires: '2022-11-28T20:53:06Z',
-    //     allowedAction: [
-    //       'read'
-    //     ],
-    //     proof: {
-    //       type: 'Ed25519Signature2020',
-    //       created: '2021-11-28T20:53:06Z',
-    // eslint-disable-next-line max-len
-    //       verificationMethod: 'did:key:z6Mkfeco2NSEPeFV3DkjNSabaCza1EoS3CmqLb1eJ5BriiaR#z6Mkfeco2NSEPeFV3DkjNSabaCza1EoS3CmqLb1eJ5BriiaR',
-    //       proofPurpose: 'capabilityDelegation',
-    //       capabilityChain: [
-    //         'urn:zcap:root:https%3A%2F%2Fexample.com%2Fdocuments'
-    //       ],
-    // eslint-disable-next-line max-len
-    //       proofValue: 'z244yxzRuFMyGfK85QcE6UewEZ3JpGDDTCvBKuxNiwdnxF3AmsSAoVYTBPLvFpYV7SeeWB4tUBGMGTF7pka6xR3av'
-    //     }
-    //   };
-      const res = await requester.post('/documents')
-        .send({});
-      res.status.should.equal(500);
+    it('should error if missing authorization header', async () => {
+      const res = await requester.post('/documents').send({});
+      const {error} = res;
+      error.status.should.equal(500);
+      error.text.should.equal('Missing or invalid "authorization" header.');
     });
   });
 });
