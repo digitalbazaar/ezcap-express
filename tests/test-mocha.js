21551fa8fd5c115ff4ee4982ec3639418124522f
David I. Lehn <dlehn@digitalbazaar.com>
2022-06-16 00:13:12 -0400

    Dependencies require Web Crypto API.

--- tests/test-mocha.js
+++ tests/test-mocha.js
@@ -3,3 +3,11 @@
  */
 import chai from 'chai';
 global.should = chai.should();
+
+// WebCrypto polyfill if needed
+import crypto from 'node:crypto';
+import webcrypto from 'isomorphic-webcrypto';
+
+if(!crypto.webcrypto) {
+  crypto.webcrypto = webcrypto;
+}
