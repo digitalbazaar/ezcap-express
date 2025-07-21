38fc42e0c6b7dfe4178cd3306b2280645f252afe
Dave Longley <dlongley@digitalbazaar.com>
2025-07-21 18:27:20 -0400

    Update dev deps (DID driver, etc.).

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,41 +1,44 @@
 /*!
- * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Digital Bazaar, Inc. All rights reserved.
  */
-import * as didKey from '@digitalbazaar/did-method-key';
+import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 import {createRootCapability} from '@digitalbazaar/zcap';
 import {decodeSecretKeySeed} from 'bnid';
+import {driver} from '@digitalbazaar/did-method-key';
 import {Ed25519Signature2020} from '@digitalbazaar/ed25519-signature-2020';
 import {ZcapClient} from '@digitalbazaar/ezcap';
 
-const didKeyDriver = didKey.driver();
+export const didKeyDriver = driver();
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'z6Mk',
+  fromMultibase: Ed25519Multikey.from
+});
 
 export async function getInvocationSigner({seed}) {
-  const decoded = decodeSecretKeySeed({secretKeySeed: seed});
-  const {methodFor} = await didKeyDriver.generate({seed: decoded});
-
-  const invocationCapabilityKeyPair = methodFor(
-    {purpose: 'capabilityInvocation'});
-  return invocationCapabilityKeyPair.signer();
+  const {methodFor} = await _fromSeed({seed});
+  const keyPair = methodFor({purpose: 'capabilityInvocation'});
+  const signer = await keyPair.signer();
+  signer.controller = keyPair.controller;
+  return signer;
 }
 
 export async function getDelegationSigner({seed}) {
-  const decoded = decodeSecretKeySeed({secretKeySeed: seed});
-  const {methodFor} = await didKeyDriver.generate({seed: decoded});
-
-  const delegationCapabilityKeyPair = methodFor(
-    {purpose: 'capabilityDelegation'});
-  return delegationCapabilityKeyPair.signer();
+  const {methodFor} = await _fromSeed({seed});
+  const keyPair = methodFor({purpose: 'capabilityDelegation'});
+  const signer = await keyPair.signer();
+  signer.controller = keyPair.controller;
+  return signer;
 }
 
 export async function delegate({seed, rootInvocationTarget, controller}) {
-  const delegationSigner = await getInvocationSigner({seed});
+  const delegationSigner = await getDelegationSigner({seed});
   const zcapClient = new ZcapClient({
     SuiteClass: Ed25519Signature2020,
     delegationSigner
   });
   // key ID is always `<controller>#...`
   const {id: keyId} = delegationSigner;
-  const rootController = keyId.substr(0, keyId.indexOf('#'));
+  const rootController = keyId.slice(0, keyId.indexOf('#'));
   const rootZcap = createRootCapability({
     controller: rootController,
     invocationTarget: rootInvocationTarget
@@ -45,4 +48,21 @@ export async function delegate({seed, rootInvocationTarget, controller}) {
     controller,
     invocationTarget: rootZcap.invocationTarget
   });
+}
+
+async function _fromSeed({seed}) {
+  const bytes = decodeSecretKeySeed({secretKeySeed: seed});
+  const keyPair = await Ed25519Multikey.generate({seed: bytes});
+  const {didDocument, methodFor, keyPairs} = await didKeyDriver.fromKeyPair({
+    verificationKeyPair: keyPair
+  });
+  // use `keyPair` in map to include secret key material
+  for(const [key, value] of keyPairs) {
+    if(!keyPair.id) {
+      keyPair.id = value.id;
+      keyPair.controller = value.controller;
+    }
+    keyPairs.set(key, keyPair);
+  }
+  return {didDocument, methodFor, keyPairs};
 }
