87ed92d7ea4c52a629369e1be28ba05d82f99d5a
Dave Longley <dlongley@digitalbazaar.com>
2025-07-21 18:30:18 -0400

    Remove unnecessary webcrypto polyfill.

--- tests/test-mocha.js
+++ tests/test-mocha.js
@@ -1,13 +1,5 @@
 /*!
- * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Digital Bazaar, Inc. All rights reserved.
  */
 import chai from 'chai';
 global.should = chai.should();
-
-// WebCrypto polyfill if needed
-import crypto from 'node:crypto';
-import webcrypto from 'isomorphic-webcrypto';
-
-if(!crypto.webcrypto) {
-  crypto.webcrypto = webcrypto;
-}
